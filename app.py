aeda1072dd5c0e739de703d59b94e4142392c79f
rishimittal8287 <rishimittal8287@gmail.com>
2026-07-30 10:39:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,10 @@ st.sidebar.image("bg.png")
 
 # ======================
 
-GOOGLE_API_KEY = st.sidebar.text_input("Tavily-API",type = "password")
+GOOGLE_API_KEY = st.sidebar.text_input("Gemini-API",type = "password")
 GROQ_API_KEY = st.sidebar.text_input("Groq-API",type = "password")
-TAVILY_API_KEY = st.sidebar.text_input("Gemini-API",type = "password")
+TAVILY_API_KEY = st.sidebar.text_input("Tavily-API",type = "password")
+
 
 #=========================
 model = ChatGoogleGenerativeAI(
