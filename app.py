3fc4fb2f55b37ca5a128c4ea7d61f719083a9f6e
rishimittal8287 <rishimittal8287@gmail.com>
2026-07-30 10:28:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ st.set_page_config(layout="wide")
 st.title("AI RESUME GENERATOR")
 st.write("""this app helps user to build customized professional resume with latest job apply links""")
 st.image("bg.png")
+st.sidebar.title("fill important details")
+st.sidebar.image("bg.png")
 
 # ======================
 
