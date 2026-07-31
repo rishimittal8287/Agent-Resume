e2f175d6aecdd7f195d5ab3417b29811a87edc74
rishimittal8287 <rishimittal8287@gmail.com>
2026-07-31 09:37:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,6 +129,6 @@ if st.button("Generate Resume"):
              st.html(code,width="stretch",
              unsafe_allow_javascript=True)
 st.divider()
-job_code = get_jobs(agent,location,profile)
+job_code =get_jobs(agent,location,profile)
 st.html(job_code, width="stretch",
         unsafe_allow_javascript=True)
